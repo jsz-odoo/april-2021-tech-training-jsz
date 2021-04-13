c31b66eaf0fe5b05268d0ed0f37a795a90c46d07
Johnny Sanchez <jsz@odoo.com>
2021-04-13 17:24:38 +0000

    [ADD] Added manifest and init.

---
+++ art_gallery/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'Art Gallery',
+    
+    "summary": """
+        An app to manage art pieces for the Gallery.
+    """,
+    
+    "description": """
+        An art gallery management app with the following features:
+        - Create, edit, delete art pieces.
+        - View art piece details.
+    """,
+    
+    "author": "Johnny Sanchez",
+    
+    "category": "Custom Development",
+    
+    "version": "0.1",
+    
+    "depends": ["base"],
+    
+    "data": [
+        
+    ],
+    
+    "demo": [
+        
+    ],
+    
+    "license": "OEEL-1",
+}
