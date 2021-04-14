6eb9ddf7d89c94bea3288341e34c82dc3cd30dc9
Johnny Sanchez <jsz@odoo.com>
2021-04-14 21:15:21 +0000

    [ADD] Added views and demo data for module.

--- art_gallery/models/art_work.py
+++ art_gallery/models/art_work.py
@@ -5,10 +5,30 @@ from odoo import api, fields, models, _
 class ArtWork(models.Model):
     _name = 'art.work'
     _description = 'Art Work'
+    _rec_name = 'nombre'
         
     nombre = fields.Char(string='Nombre', required=True)
     largo = fields.Float(string='Largo', default=1.0)
     anchura = fields.Float(string='Anchura', default=1.0)
     solo_admin = fields.Boolean(string='Opción para Administradores', groups="base.group_system")
-    foto_obra = fields.Image(string='Fotografía de la Obra')
+    foto_obra = fields.Image(string='Fotografía de la Obra', max_width=800, max_height=600)
+    state = fields.Selection(string="Estado", selection=[
+        ('almacenado', 'Almacenado'),
+        ('exhibicion', 'En exhibición'),
+        ('vendido', 'Vendido')
+    ], default='almacenado')
+    peso = fields.Float(string="Peso", states={
+        'almacenado': [('required', True)],
+        'exhibicion': [('readonly', True)],
+        'vendido': [('invisible', True)]
+    })
+    precio = fields.Float(string="Precio", required=True)
     
+    def almacenar(self):
+        self.state = 'almacenado'
+        
+    def exhibir(self):
+        self.state = 'exhibicion'
+        
+    def vender(self):
+        self.state = 'vendido'
